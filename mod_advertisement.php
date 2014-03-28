4e164c24c506ebf90376a15ec5bb19b2fbfb42dc
Gregory Daynes <gregdaynes@gmail.com>
2014-03-27 17:11:35 -0700

    launched code

---
+++ mod_advertisement.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Advertisement Module Entry Point
+ *
+ * @package    jevolve.extensions
+ * @subpackage Modules
+ * @link       http://jevolve.net
+ * @license    GNU/GPL, see LICENSE.php
+ * mod_stack is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$template = $params->get('template', 'default');
+
+require( JModuleHelper::getLayoutPath( 'mod_advertisement', $template ) );
