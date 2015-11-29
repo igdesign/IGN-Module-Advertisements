bcf96e69260ca0d90e146dfa8c4275ddcfa6baa2
Gregory Daynes <gregdaynes@gmail.com>
2015-11-29 13:44:13 -0800

    restructuring

---
+++ gulp-tasks/default.js
@@ -0,0 +1,14 @@
+(function() {
+'use strict';
+
+// Dependencies ===============
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var config = require('../gulp-config');
+
+// Stream =====================
+gulp.task('default', function() {
+
+});
+
+})();
